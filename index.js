447f9c4b81a8430b3b2f972adc3854f393dd3cef
Дмитрий Маклаков <dimonabinsk@yandex.ru>
2023-01-29 23:54:18 +0300

    Вывод HTML-страницы

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const http = require("http");
 const chalk = require("chalk");
+const fs = require("fs/promises");
+const path = require("path");
 
 const port = 3000;
+const basePath = path.join(__dirname, "pages");
 
-const server = http.createServer((req, res) => {
-    console.log("Request method", req.method);
-    console.log("Request url", req.url);
-    res.end("Hello from server!!");
+const server = http.createServer(async (req, res) => {
+  if (req.method === "GET") {
+    const content = await fs.readFile(path.join(basePath, "index.html"));
+    // res.setHeader("Content-Type", "text/html");
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+    res.end(content);
+  }
 });
 
 server.listen(port, () => {
-  console.log(chalk.green(`Server has been started on port:${port}`));
+  console.log(chalk.green(`Сервер запущен на порте: ${port}`));
 });
